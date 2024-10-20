fe19a9f19317bcf46acb9903e7d01b68e2b5c062
Dani170907 <daniramadhan170907@gmail.com>
2024-10-20 16:09:25 +0700

    mengirim data title dan name ke halaman yang ada diviews dan mengirim kembali ke component layout diambil oleh nav-link dan navbar

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
     <h3 class="text-xl">About ME</h3>
+    <p>Owner: {{ $name }}</p>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,14 @@ Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
-Route::get('/about', function () {
-    return view('about', ['title' => 'About Page']);
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog Page']);
+Route::get('/about', function () {
+    return view('about', ['name' => 'Dani Ramadhan','title' => 'About Page']);
 });
+
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact Page']);
