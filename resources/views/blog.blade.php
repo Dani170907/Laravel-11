f95ee4c7ae5dbc8f43c9d4f04191a49157003fea
Dani170907 <daniramadhan170907@gmail.com>
2024-10-23 05:43:41 +0700

    membuat halaman blog dengan isi konten artikel yang masih statis

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,28 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    <h3 class="text-xl">Welcome to my Blog</h3>
+
+    
+<article class="py-8 max-w-screen-md border-b border-gray-300">
+    <h2  class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 1</h2>
+    <div class="text-gray-500 text-base">
+        <a href="">Dani Ramadhan</a> | 23 Oktober 2024
+    </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo nostrum unde, 
+            harum dolor, obcaecati similique expedita ex quam doloribus ullam quia soluta illo, 
+            exercitationem nulla aliquid quisquam quasi? Deleniti, ipsa.
+        </p>
+    <a class="font-medium text-blue-500 hover:underline" href="">Read More &raquo;</a>
+</article>
+
+<article class="py-8 max-w-screen-md border-b border-gray-300">
+    <h2  class="mb-1 text-3xl tracking-tight font-bold text-gray-900">Judul Artikel 2</h2>
+    <div class="text-gray-500 text-base">
+        <a href="">Dani Ramadhan</a> | 23 Oktober 2024
+    </div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Animi quibusdam ex sed laboriosam alias error quia! Eius quo enim minus eos fugiat non dolorum dignissimos ad atque, 
+        voluptatem maiores ratione?
+    </p>
+    <a class="font-medium text-blue-500 hover:underline" href="">Read More &raquo;</a>
+</article>
 </x-layout>
