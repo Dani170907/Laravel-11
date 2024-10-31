6d2fcd00ce746c189f71f099ee874cb41d724944
Dani170907 <daniramadhan170907@gmail.com>
2024-11-01 04:42:11 +0700

    memanggil method author di view untuk menampilkan nama

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    
+
 @foreach ($posts as $post)
-    
+
 <article class="py-8 max-w-screen-md border-b border-gray-300">
     <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">
@@ -11,11 +11,11 @@
     </a>
 
     <div class="text-gray-500 text-base">
-        <a href="">{{ $post['author'] }}</a> | {{ $post->created_at->format('j F Y')  }}
+        <a href="">{{ $post->author->name }}</a> | {{ $post->created_at->format('j F Y')  }}
     </div>
     <p>{{ Str::limit($post['body']), 150 }}</p>
     <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
 </article>
 @endforeach
-    
+
 </x-layout>
