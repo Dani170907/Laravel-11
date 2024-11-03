63c0c832e05a50becf327373bedd3bb585861eae
Dani170907 <daniramadhan170907@gmail.com>
2024-11-03 22:15:03 +0700

    membuat eager loading menjadi default

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,9 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', function () {
-    $posts = Post::with('author', 'category')->latest()->get();
+    // $posts = Post::with('author', 'category')->latest()->get();
+    $posts = Post::latest()->get();
+
     return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
@@ -20,15 +22,15 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
-    $posts = $user->posts->load('category', 'author');
+    // $posts = $user->posts->load('category', 'author');
 
-    return view('posts', ['title' => count($posts) . ' Articles by ' . $user->name, 'posts' => $posts]);
+    return view('posts', ['title' => count($user->posts) . ' Articles by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    $posts = $category->posts->load('category', 'author');
+    // $posts = $category->posts->load('category', 'author');
 
-    return view('posts', ['title' => 'Articles in: ' . $category->name, 'posts' => $posts]);
+    return view('posts', ['title' => 'Articles in: ' . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/about', function () {
