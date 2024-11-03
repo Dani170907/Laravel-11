dddf4e0905937f0650dcc5b929c769f26c50c9e3
Dani170907 <daniramadhan170907@gmail.com>
2024-11-03 15:27:13 +0700

    menjalankan seeder menggunakan factory yang sudah dibuat

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,27 +19,32 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Dani Ramadhan',
-            'username' => 'daniramadhan',
-            'email' => 'daniramadhan170907@gmail.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('password'),
-            'remember_token' => Str::random(10)
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Dani Ramadhan',
+        //     'username' => 'daniramadhan',
+        //     'email' => 'daniramadhan170907@gmail.com',
+        //     'email_verified_at' => now(),
+        //     'password' => Hash::make('password'),
+        //     'remember_token' => Str::random(10)
+        // ]);
 
-        Category::factory()->create([
-            'name' => 'Web Design',
-            'slug' => 'web-design'
-        ]);
+        // Category::factory()->create([
+        //     'name' => 'Web Design',
+        //     'slug' => 'web-design'
+        // ]);
 
-        Post::factory()->create([
-            'title' => 'Judul Artikel 1',
-            'author_id' => 1,
-            'category_id' => 1,
-            'slug' => 'judul-artikel-1',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut repellat labore natus. Voluptatum rerum maxime itaque, esse quisquam aspernatur accusamus fuga quasi vero? Non, quisquam? Excepturi itaque possimus fugiat repudiandae?'
-        ]);
+        // Post::factory()->create([
+        //     'title' => 'Judul Artikel 1',
+        //     'author_id' => 1,
+        //     'category_id' => 1,
+        //     'slug' => 'judul-artikel-1',
+        //     'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut repellat labore natus. Voluptatum rerum maxime itaque, esse quisquam aspernatur accusamus fuga quasi vero? Non, quisquam? Excepturi itaque possimus fugiat repudiandae?'
+        // ]);
+
+        Post::factory(100)->recycle([
+            Category::factory(3)->create(),
+            User::factory(5)->create()
+        ])->create();
     }
 }
 
