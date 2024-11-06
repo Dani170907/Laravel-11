6e03e591a11c0d5c0a3e0d4dba7f319722341e79
Dani170907 <daniramadhan170907@gmail.com>
2024-11-06 20:06:06 +0700

    menginstall dan konfigurasi flowbite

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css','resources/js/app.js'])
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <title>Halaman Home</title>
 </head>
@@ -13,15 +13,15 @@
 <div class="min-h-full">
 
     <x-navbar></x-navbar>
-  
+
     <x-header>{{ $title }}</x-header>
-    
+
     <main>
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         {{ $slot }}
       </div>
     </main>
-  </div>  
-  
+  </div>
+
 </body>
 </html>
