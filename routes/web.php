83098c24d0ad9fd47d57187fd1f1a1cc8d336ea7
Dani170907 <daniramadhan170907@gmail.com>
2024-11-07 07:41:11 +0700

    menambahkan fitur pencarian pada halaman posts

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,13 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', function () {
+    $posts = Post::latest();
 
+    if (request('search')) {
+        $posts->where('title','like','%'. request('search') .'%');
+    }
 
-    $posts = Post::latest()->get();
-
-    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+    return view('posts', ['title' => 'Blog', 'posts' => $posts->get()]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
