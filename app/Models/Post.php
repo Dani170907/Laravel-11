bf873192950bf76c8df256b0cc4a09b5a3842b5c
Dani170907 <daniramadhan170907@gmail.com>
2024-11-07 10:54:32 +0700

    mengubah fitur pencarian agar lebih dinamis dengan menggunakan when

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,8 +26,8 @@ class Post extends Model
 
     public function scopeFilter(Builder $query, array $filters): void
     {
-        if (isset($filters['search']) ?? false) {
+        $query->when($filters['search'] ?? false, function($query, $search){
             $query->where('title', 'like', '%' . request('search') . '%');
-        }
+        });
     }
 }
